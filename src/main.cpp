02f148911ef66adce70f8da6385c19f527fd4f06
festevil <62298846+festevil@users.noreply.github.com>
2023-01-29 20:29:05 +0800

    Update files

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "storage.h"
+#include "types.h"
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    int BLOCKSIZE = 200; // Set BlockSize to 200B
+
+
+    return 0;
+}
