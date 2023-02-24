970da80d0405556747405de48ee6cfcf3578ce61
festevil <ben.tanjq@gmail.com>
2023-02-24 22:16:27 +0800

    Initialization algo done, testing needed

--- src/bpt.cpp
+++ src/bpt.cpp
@@ -16,7 +16,7 @@ void BPT::setRoot(char *pRoot){
 }
 
 // Keylist and addresslist are sorted
-void BPT::initializeBPT(int[] keylist, char*[] addresslist){
+void BPT::initializeBPT(vector<int> keylist, vector<char *> addresslist){
     if (intialized_){
         std::cout << "Already initialized!" << endl;
         return;
@@ -30,18 +30,18 @@ void BPT::initializeBPT(int[] keylist, char*[] addresslist){
     vector<int> templowerbounds;
     size_t count = 0;
     BPTNode leafnode = new BPTNode(root_);
-    BPTNode prevnode;
-    char *firstnodeaddress = root_;
+    BPTNode prevNode;
     int curKeyCount = 0;
     bool setmax = false;
     //Initialize leaf nodes
-    while (count < keylist.size){
-        if (leafnode.NumKeys() == maxKeyCount){
+    addresses.push_back(root_);
+    while (count < keylist.size()){
+        if (leafnode.GetNumKeys() == maxKeyCount){
             
             
-            prevnode = leafnode;
+            prevNode = leafnode;
             char *nodeAddress = storage_.AddBlock();
-            leafnode -> children_ + MAX_KEYS = nodeAddress;
+            leafnode.SetChild(MAX_KEYS, nodeAddress);
             leafnode = new BPTNode(nodeAddress);
             nodes.push_back(leafnode);
             addresses.push_back(nodeAddress);
@@ -49,11 +49,11 @@ void BPT::initializeBPT(int[] keylist, char*[] addresslist){
             curKeyCount = 0;
 
             //ensure minimum no of keys
-            if((keylist.size - count) <= (2*maxKeyCount)){
+            if((keylist.size() - count) <= (2*maxKeyCount)){
                 if (!setmax){
                     setmax = true;
-                    if ((keylist.size - count - maxKeyCount) < floor((maxKeyCount + 1)/2)){
-                        maxKeyCount = keylist.size - count - floor((maxKeyCount + 1)/2);
+                    if ((keylist.size() - count - maxKeyCount) < floor((maxKeyCount + 1)/2)){
+                        maxKeyCount = keylist.size() - count - floor((maxKeyCount + 1)/2);
                     }
                 }
             }
@@ -63,11 +63,50 @@ void BPT::initializeBPT(int[] keylist, char*[] addresslist){
         count++;
     }
     
-    while (nodes.size > 0){
+    while (nodes.size() > 0){
         maxKeyCount = MAX_KEYS;
         setmax = false;
         count = 0;
-        for (int i = 0; i<nodes.size; i++){}
+        char *nodeAddress = storage_.AddBlock();
+        BPTNode nonleafNode = new BPTNode(nodeAddress);
+        nonleafNode.SetChild(0, addresses[0]);
+        tempaddresses.push_back(nodeAddress);
+        curKeyCount = 0;
+        for (int i = 0; i<nodes.size(); i++){
+            if (nonleafNode.GetNumKeys() == maxKeyCount){
+                    prevNode = nonleafNode;
+                    
+                    char *nodeAddress = storage_.AddBlock();
+                    BPTNode nonleafNode = new BPTNode(nodeAddress);
+                    tempaddresses.push_back(nodeAddress);
+                    nonleafNode.InsertChild(0, addresses[i + 1]);
+                    templowerbounds.push_back(lowerbounds[i]);
+                    tempnodes.push_back(nonleafNode);
+                    curKeyCount = 0;
+
+                    if((keylist.size() - count) <= (2*(maxKeyCount+1))){
+                        if (!setmax){
+                            setmax = true;
+                            if ((keylist.size() - count - maxKeyCount - 1) < (floor((maxKeyCount)/2) + 1)){
+                                maxKeyCount = keylist.size() - count - floor((maxKeyCount)/2) - 1;
+                            }
+                        }
+                    }
+            }
+            else {
+                nonleafNode.InsertKey(curKeyCount, lowerbounds[i]);
+                nonleafNode.InsertChild(curKeyCount + 1, addresses[i + 1]);
+                curKeyCount++;
+            }
+
+            vector<BPTNode *> nodes(tempnodes);
+            vector<char *> addresses(tempaddresses);
+            vector<int> lowerbounds(templowerbounds);
+            tempnodes.clear();
+            tempaddresses.clear();
+            lowerbounds.clear();
+        }
+        root_ = addresses[0];
     }
 
     
